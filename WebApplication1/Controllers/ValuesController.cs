a7d5ab06a7e62ed7bb1458803ff6985f35df74fb
Beatriz Tavares <beatriztavares@gmail.com>
2017-06-20 20:59:11 -0300

    changing post 5

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace WebApplication1.Controllers
 {
