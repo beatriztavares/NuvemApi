84a89b1d5500695cde143b6c0111d72d02d32a29
Beatriz Tavares <beatriztavares@gmail.com>
2017-06-20 22:37:01 -0300

    change push 5

--- WebApplication1/WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,9 +9,36 @@ using System.Web.Http;
 
 namespace WebApplication1.Controllers
 {
-    [Authorize]
+   
     public class ValuesController : ApiController
     {
+        static CloudQueue cloudQueue;
+
+        public ValuesController()
+        {
+            Connect();
+        }
+
+        public void Connect()
+        {
+            var connectionString = "DefaultEndpointsProtocol=https;AccountName=nuvempuc;AccountKey=5Xl8yKJmsI0Ud0+GiZEqhydILreQIXxOLJqhcrl81xp7PLIpQJK836/cb2DfTmPREDkkdxjNw9HJeuwFCDcF6Q==;EndpointSuffix=core.windows.net";
+
+            CloudStorageAccount cloudStorageAccount;
+
+            if (!CloudStorageAccount.TryParse(connectionString, out cloudStorageAccount))
+            {
+                Console.WriteLine("Expected connection string 'Azure Storage Account Demo Primary' to be a valid Azure Storage Connection String.");
+            }
+
+            var cloudQueueClient = cloudStorageAccount.CreateCloudQueueClient();
+            cloudQueue = cloudQueueClient.GetQueueReference("queue1");
+
+            // Note: Usually this statement can be executed once during application startup or maybe even never in the application.
+            //       A queue in Azure Storage is often considered a persistent item which exists over a long time.
+            //       Every time .CreateIfNotExists() is executed a storage transaction and a bit of latency for the call occurs.
+            //cloudQueue.CreateIfNotExists();
+        }
+
         // GET api/values
         public IEnumerable<string> Get()
         {
@@ -25,6 +54,9 @@ namespace WebApplication1.Controllers
         // POST api/values
         public string Post([FromBody]string value)
         {
+            var message = new CloudQueueMessage(value);
+
+            cloudQueue.AddMessage(message);
             return "Post!";
         }
 
